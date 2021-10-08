7045d543c93c8c9cd9f5299b72a1a24bd5110bdd
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-08 18:02:46 +0100

    Added fraudulent_activity_notifications solution

--- src/InterviewPreparationKit/Sorting/Medium/fraudulent_activity_notifications.py
+++ src/InterviewPreparationKit/Sorting/Medium/fraudulent_activity_notifications.py
@@ -5,4 +5,28 @@
 # @param d           Days for median spending.
 # @return The number of notices sent.
 def activity_notifications(expenditure: list, d: int):
-    pass
+    f = [0] * 201
+    count = 0
+    for i in expenditure[:d]:
+        f[i] += 1
+    for i, v in enumerate(expenditure[d:]):
+        limit = get_limit(f, d)
+        if v >= limit:
+            count += 1
+        f[v] += 1
+        f[expenditure[i]] -= 1
+    return count
+
+
+def get_limit(f: list, d: int):
+    count = 0
+    m1, m2 = (d // 2, d // 2 + 1)
+    m = []
+    for i, j in enumerate(f):
+        count += j
+        if not m and m1 <= count:
+            m.append(i)
+        if m2 <= count:
+            m.append(i)
+            break
+    return m[-1] * 2 if d % 2 else sum(m)
