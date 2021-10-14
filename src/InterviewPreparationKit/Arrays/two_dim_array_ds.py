74977674c5c748485a5787853963821363b4c7d6
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-14 11:46:49 +0100

    Added two_dim_array_ds solution

--- src/InterviewPreparationKit/Arrays/two_dim_array_ds.py
+++ src/InterviewPreparationKit/Arrays/two_dim_array_ds.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def hourglass_sum(arr: list):
     """
     Hour Glass Sum problem: https://www.hackerrank.com/challenges/2d-array/problem
@@ -5,5 +8,24 @@ def hourglass_sum(arr: list):
     :param arr: An array of integers.
     :return: The maximum hourglass sum.
     """
+    largest_sum = -sys.maxsize
+    for i in range(0, len(arr[0]) - 2):
+        for j in range(0, len(arr) - 2):
+            largest_sum = max(largest_sum, sum_two_dim_array(get_hour_glass(arr, i, j)))
+    return largest_sum
 
-    return -2
+
+def get_hour_glass(arr: list, x: int, y: int):
+    if x >= len(arr[0]) or y >= len(arr):
+        return None
+    return [[arr[x][y + i] for i in range(0, 3)],
+            [0, arr[x + 1][y + 1], 0],
+            [arr[x + 2][y + i] for i in range(0, 3)]]
+
+
+def sum_two_dim_array(two_dim_array: list):
+    total = 0
+    for i in two_dim_array:
+        for j in i:
+            total += j
+    return total
