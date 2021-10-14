aa777bb20fc60276defb846fde400b8d769d8e5b
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-14 14:22:03 +0100

    Added number_line_jumps solution

--- src/ProblemSolving/Implementation/number_line_jumps.py
+++ src/ProblemSolving/Implementation/number_line_jumps.py
@@ -8,4 +8,27 @@ def kangaroo(x1: int, v1: int, x2: int, v2: int):
     :param v2: Jump distance for kangaroo 2.
     :return: Either YES or NO.
     """
-    return "NULL"
+    yes = "YES"
+    no = "NO"
+    if v1 == v2:
+        return yes if x1 == x2 else no
+    elif x1 == x2:
+        return no
+    elif x1 > x2:
+        return yes if same_location_possible(x1, v1, x2, v2) else no
+    else:
+        return yes if same_location_possible(x2, v2, x1, v1) else no
+
+
+def same_location_possible(lead_start: int, lead_jump: int, tail_start: int, tail_jump: int):
+    if lead_jump <= tail_jump:
+        lead_pos = lead_start
+        tail_pos = tail_start
+        diff = lead_start - tail_start
+        while True:
+            if diff <= 0:
+                return diff == 0
+            lead_pos += lead_jump
+            tail_pos += tail_jump
+            diff = lead_pos - tail_pos
+    return False
