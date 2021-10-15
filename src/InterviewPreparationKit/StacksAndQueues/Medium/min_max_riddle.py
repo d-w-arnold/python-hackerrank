0094968a813527fc550e9b876d5b1a6183d3bb6d
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-15 15:10:51 +0100

    Added min_max_riddle solution

--- src/InterviewPreparationKit/StacksAndQueues/Medium/min_max_riddle.py
+++ src/InterviewPreparationKit/StacksAndQueues/Medium/min_max_riddle.py
@@ -5,4 +5,28 @@ def riddle(arr: list) -> list:
     :param arr: An array of 'n' integers.
     :return: An array of integers representing the maximum minimum value for each window size from 1 to 'n'.
     """
-    return [-2]
+    n = len(arr)
+    s = []
+    left = ([-1] * n) + [0]
+    right = ([n] * n) + [0]
+    for i in range(0, n):
+        while s and arr[s[-1]] >= arr[i]:
+            s.pop()
+        if s:
+            left[i] = s[-1]
+        s.append(i)
+    while s:
+        s.pop()
+    for i in range(n - 1, -1, -1):
+        while s and arr[s[-1]] >= arr[i]:
+            s.pop()
+        if s:
+            right[i] = s[-1]
+        s.append(i)
+    max_min = [0] * (n + 1)
+    for i in range(0, n):
+        ln = right[i] - left[i] - 1
+        max_min[ln] = max(max_min[ln], arr[i])
+    for i in range(n - 1, 0, -1):
+        max_min[i] = max(max_min[i], max_min[i + 1])
+    return max_min[1:]
