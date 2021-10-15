c86fadf75f8d68bf80030c37c1c47fd685154245
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-15 15:24:45 +0100

    Added sales_by_merchant solution

--- src/ProblemSolving/Implementation/sales_by_merchant.py
+++ src/ProblemSolving/Implementation/sales_by_merchant.py
@@ -6,4 +6,15 @@ def sock_merchant(n: int, ar: list) -> int:
     :param ar: The colors of each sock.
     :return: The number of pairs.
     """
-    return -2
+    pairs = 0
+    socks = {}
+    for i in range(0, n):
+        a = ar[i]
+        if a in socks:
+            value = socks[a]
+            if value % 2 == 1:
+                pairs += 1
+            socks[a] = value + 1
+        else:
+            socks[a] = 1
+    return pairs
