eeffd3b1232a3b1ea8038f3b2e7bc98b25ee90a6
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-15 12:51:16 +0100

    Added plus_minus solution

--- src/ProblemSolving/Warmup/plus_minus.py
+++ src/ProblemSolving/Warmup/plus_minus.py
@@ -4,4 +4,16 @@ def plus_minus(arr: list):
 
     :param arr: An array of integers.
     """
-    print("NULL")
+    pos_num = 0
+    neg_num = 0
+    zeros = 0
+    for i in arr:
+        if i > 0:
+            pos_num += 1
+        elif i < 0:
+            neg_num += 1
+        else:
+            zeros += 1
+    print(f"{pos_num / len(arr):.6f}")
+    print(f"{neg_num / len(arr):.6f}")
+    print(f"{zeros / len(arr):.6f}")
