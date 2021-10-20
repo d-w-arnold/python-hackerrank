4513421175514463a4f00455027fbd8dafa4040f
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-20 16:14:30 +0100

    Started greedy_florist solution

---
+++ src/InterviewPreparationKit/GreedyAlgorithms/Medium/greedy_florist.py
@@ -0,0 +1,9 @@
+def get_min_cost(k: int, c: list) -> int:
+    """
+    Greedy Florist problem: https://www.hackerrank.com/challenges/greedy-florist/problem
+
+    :param k: The number of friends.
+    :param c: The original price of each flower.
+    :return: The minimum cost to purchase all flowers.
+    """
+    return -2
