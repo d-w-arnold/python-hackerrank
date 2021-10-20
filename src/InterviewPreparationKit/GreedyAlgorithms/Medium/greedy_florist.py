35f5c52bf35f91aebab2baa9e3881c351e05c692
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-20 16:17:23 +0100

    Added greedy_florist solution

--- src/InterviewPreparationKit/GreedyAlgorithms/Medium/greedy_florist.py
+++ src/InterviewPreparationKit/GreedyAlgorithms/Medium/greedy_florist.py
@@ -6,4 +6,13 @@ def get_min_cost(k: int, c: list) -> int:
     :param c: The original price of each flower.
     :return: The minimum cost to purchase all flowers.
     """
-    return -2
+    total = 0
+    c.sort()
+    num_of_purchases = 0
+    num_of_group_purchases = -1
+    for i in range(len(c) - 1, -1, -1):
+        if num_of_purchases % k == 0:
+            num_of_group_purchases += 1
+        num_of_purchases += 1
+        total += (num_of_group_purchases + 1) * c[i]
+    return total
