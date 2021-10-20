0b202a8c3fc7f424369a553b24b8c9782da739f3
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-20 15:54:32 +0100

    Added time_conversion solution

--- src/ProblemSolving/Warmup/time_conversion.py
+++ src/ProblemSolving/Warmup/time_conversion.py
@@ -5,4 +5,17 @@ def time_conversion(s: str) -> str:
     :param s: A time in 12-hour format.
     :return: The time in 24-hour format.
     """
-    return "NULL"
+    time_sep = ":"
+    am_pm = s[len(s) - 2:]
+    time = s[:len(s) - 2]
+    hour_min_sec = time.split(time_sep)
+    if int(hour_min_sec[0]) == 12:
+        if am_pm == "PM":
+            return time
+        elif am_pm == "AM":
+            hour_min_sec[0] = "00"
+    elif am_pm == "PM" and 1 <= int(hour_min_sec[0]) <= 11:
+        hour_min_sec[0] = str(int(hour_min_sec[0]) + 12)
+    else:
+        return time
+    return time_sep.join(hour_min_sec)
