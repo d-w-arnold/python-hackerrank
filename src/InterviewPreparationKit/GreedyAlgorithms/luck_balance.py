ddf2336a38cb50105a979141af2371dca0a03449
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-28 16:50:50 +0100

    Started luck_balance solution

---
+++ src/InterviewPreparationKit/GreedyAlgorithms/luck_balance.py
@@ -0,0 +1,9 @@
+def luck_balance(k: int, contests: list) -> int:
+    """
+    Luck Balance problem: https://www.hackerrank.com/challenges/luck-balance/problem
+
+    :param k: The number of important contests Lena can lose.
+    :param contests: A 2D array of integers where each contests[i] contains two integers that represent the luck balance and importance of the ith contest.
+    :return: The maximum luck balance achievable.
+    """
+    return -2
