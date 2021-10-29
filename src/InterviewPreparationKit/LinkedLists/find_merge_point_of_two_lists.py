552241e95c1f25965138ef1cd61b5d259c05f013
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-29 16:18:16 +0100

    Added find_merge_point_of_two_lists solution

--- src/InterviewPreparationKit/LinkedLists/find_merge_point_of_two_lists.py
+++ src/InterviewPreparationKit/LinkedLists/find_merge_point_of_two_lists.py
@@ -31,4 +31,47 @@ def find_merge_node(head1: SinglyLinkedListNode, head2: SinglyLinkedListNode) ->
     :param head2: A reference to the head of the second list.
     :return: The data value of the node where the lists merge.
     """
-    return -2
+    if head1.next and head2.next:
+        ans = -1
+        curr1, curr2 = head1.next, head2.next
+        c1_conn = c2_conn = False
+        while True:
+            if ans == -1:
+                if c1_conn == c2_conn and curr1.data == curr2.data:
+                    ans = curr1.data
+                    continue
+                if not curr1.next and not curr2.next:
+                    break
+                if not curr1.next and not c1_conn:
+                    curr1 = head2
+                    c1_conn = True
+                else:
+                    curr1 = curr1.next
+                if not curr2.next and not c2_conn:
+                    curr2 = head1
+                    c2_conn = True
+                else:
+                    curr2 = curr2.next
+            else:
+                if curr1.data != curr2.data:
+                    ans = -1
+                    continue
+                elif not curr1.next and not curr2.next:
+                    break
+                elif not curr1.next or not curr2.next:
+                    ans = -1
+                    break
+                curr1 = curr1.next
+                curr2 = curr2.next
+        return ans
+    elif head1.next:
+        while head1:
+            if head1.data == head2.data:
+                return head1.data
+            head1 = head1.next
+    else:
+        while head2:
+            if head2.data == head1.data:
+                return head2.data
+            head2 = head2.next
+    return -1
